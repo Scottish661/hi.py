0dbbd584f95261cd40b6dfa1ef71972f67ffbce5
Scottish661 <h0785014@gmail.com>
2026-09-21 04:10:59 +0100

    Create RPG.py

---
+++ RPG.py
@@ -0,0 +1,10 @@
+class person:
+ def __init__(this,health,attack,speed):
+  this.health = health
+  this.attack = attack
+  this.speed = speed
+ 
+person.health_pro = person(50,30,30)
+person.attack_pro = person(30,50,30)
+person.speed_pro = person(30,30,50)
+person.balance = person(40,40,40)
