8857c3c66e4cfacf747107a123167fabcdd6c236
Scottish661 <h0785014@gmail.com>
2026-09-23 19:20:45 +0100

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,21 @@
+from tkinter import *
+game_width = 700
+game_height = 700
+speed = 50
+space_size = 3
+snake_game = "#00FF00"
+food_colour = "#FF0000"
+background_colour = "#000000"
+class Snake:
+    pass
+class Food:
+    pass
+def next_turn():
+    pass
+def move(new move):
+    pass
+def hit_box():
+    pass
+    pass
+def game_over():
+    pass
